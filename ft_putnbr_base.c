88645bc7867950407d910d8f8f53e9a712849ed7
Julien AZOUZ <juazouz@e3r3p10.42.fr>
2018-11-30 19:11:01 +0100

    Fix ft_putnbr_base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tavelino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 16:06:45 by tavelino          #+#    #+#             */
-/*   Updated: 2018/11/30 17:22:29 by tavelino         ###   ########.fr       */
+/*   Updated: 2018/11/30 19:10:46 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-#include "stdio.h"
 
-void	ft_strrev(char *str);
-
-void	ft_convertnbr_base(int nbr, char* base, int base_len, char* str)
+void	ft_convertnbr_base(int nbr, char* base, int base_len, char **str)
 {
-	
-	if(nbr < 0)
-		ft_putchar('-');
-	else
+
+	if (nbr < 0)
+	{
+		**str = '-';
+		(*str)++;
 		nbr = -nbr;
-	if(nbr <= -base_len)
+	}
+	if (nbr > base_len)
 	{
 		//recursive
-		ft_convertnbr_base(-(nbr / base_len), base, base_len, str + 1);	
+		ft_convertnbr_base((nbr / base_len), base, base_len, str);
 	}
-	else
-		str[1] = '\0';
-	*str = base[-(nbr % base_len)];
+	// else
+	// 	str[1] = '\0';
+	**str = base[(nbr % base_len)];
+	(*str)++;
 }
 
 
@@ -38,15 +38,13 @@ int	ft_putnbr_base(size_t nbr, char *base)
 {
 	int len;
 	char str[50];
+	char	*str2;
 
+	str2 = str;
 	len = ft_strlen(base);
-	ft_convertnbr_base(nbr, base, len, str);
+	ft_bzero(str, sizeof(str));
+	ft_convertnbr_base(nbr, base, len, &str2);
 	//ft_strrev(str); //need to make a function to reverse before print ft_strrev.c
 	ft_putstr(str);
-	return (ft_strlen(str)); // return size
-}
-
-int		main()
-{
-	printf("\nsize is: %d \n", ft_putnbr_base(6, "01"));
+	return (str2 - str); // return size
 }
